220544f2871747631d5bd1556d1c814bf6108c46
bg117 <76200573+bg117@users.noreply.github.com>
2024-07-30 20:26:36 +0800

    add admin-login page

--- components/Card.js
+++ components/Card.js
@@ -1,7 +1,8 @@
 export default function Card(props) {
+    const {className, children} = props;
     return (
-        <div className="rounded-lg border border-gray-400 dark:border-brand-dark-lighter bg-white dark:bg-brand-dark-default flex flex-col lg:flex-row shadow-lg">
-            {props.children}
+        <div className={`rounded-lg border border-gray-400 dark:border-brand-dark-lighter bg-white dark:bg-brand-dark-default flex flex-col lg:flex-row shadow-lg ${className}`}>
+            {children}
         </div>
     )
 }
