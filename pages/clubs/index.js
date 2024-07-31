582904d0581a50aa91f0fb94d9c5536d9faf43b5
bg117 <76200573+bg117@users.noreply.github.com>
2024-07-31 21:32:40 +0800

    add constant club directory array

--- pages/clubs/index.js
+++ pages/clubs/index.js
@@ -1,31 +1,50 @@
 import Page from "@/components/Page";
 import Image from "next/image";
 
+const clubs = [
+  "BKD",
+  "BSP",
+  "CACHET",
+  "GSP",
+  "HUMSS",
+  "Hymno",
+  "Ikapati",
+  "Interact",
+  "KaPaRiz",
+  "LGDT",
+  "Music Club",
+  "OBRA",
+  "Pararayos",
+  "RCY",
+  "The Satellite",
+  "SMAP",
+  "SMS",
+  "SPARCS",
+  "SSLG",
+  "STARS",
+  "SYSTEM",
+  "Wizards of English",
+  "Y-Teens",
+  "YES-O",
+  "Youth Alert",
+];
+
 export default function Clubs() {
-  // generate 18 clubs
-  let clubs = [];
-  for (let i = 0; i < 18; i++) {
-    clubs.push({
-      name: `Club ${i}`,
-      imageUrl: `https://via.placeholder.com/150?text=Club+${i}`,
-    });
-  }
   return (
     <Page isContent>
+      <h1 className="text-4xl font-bold mb-4">Club Directory</h1>
+
       <div className="grid grid-cols-3 lg:grid-cols-8 md:grid-cols-5 gap-4">
         {clubs.map((club, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center"
-          >
+          <div key={index} className="flex flex-col items-center">
             <Image
-              src={club.imageUrl}
-              alt={club.name}
+              src={`https://via.placeholder.com/150?text=${club}`}
+              alt={club}
               width={150}
               height={150}
               className="h-auto object-cover rounded-full"
             />
-            <h2 className="text-xl font-bold">{club.name}</h2>
+            <h2 className="text-xl font-bold text-center mt-2">{club}</h2>
           </div>
         ))}
       </div>
