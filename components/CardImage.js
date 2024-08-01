417975da3aedeb6c629f86c7e9139a0454862a08
bg117 <76200573+bg117@users.noreply.github.com>
2024-08-01 22:09:36 +0800

    make card image vertical

--- components/CardImage.js
+++ components/CardImage.js
@@ -1,8 +1,16 @@
 import Image from "next/image";
 
 export default function CardImage(props) {
-    return (
-        <Image src={props.src} alt={props.alt} width={0} height={0} sizes="100vw"
-            className="h-48 w-auto lg:h-auto lg:w-48 lg:rounded-l-lg lg:rounded-tr-none rounded-t-lg object-cover" />
-    )
+  const { src, alt } = props;
+
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      width={0}
+      height={0}
+      sizes="100vw"
+      className="h-48 w-auto rounded-t-lg object-cover"
+    />
+  );
 }
