53635a1e1da2c43c80f2438c1e2315f9c49ae81e
Kian Gabriel Arambulo <76200573+bg117@users.noreply.github.com>
2024-08-07 23:31:02 +0800

    change font to Nunito

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type {Metadata} from "next";
-import { Inter } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const nextFont = Inter({subsets: ["latin"]});
+const nextFont = Nunito({subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Create Next App",
