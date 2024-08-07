2f2cee110ee129affd7eead95f97c9b575a2f0a8
Kian Gabriel Arambulo <76200573+bg117@users.noreply.github.com>
2024-08-07 21:15:55 +0800

    add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 import {Header} from "@/components/Header";
+import {Logo} from "@/components/MainNav";
 
 export default function Home() {
     return (
-        <Header />
+        <div className="flex flex-col min-h-screen">
+            <Header/>
+            <div
+                className="flex flex-col flex-1 gap-4 bg-gradient-to-tr from-brand-blue-darker to-brand-red-darker justify-center items-center text-white p-4">
+                <Logo/>
+                <h1 className="text-4xl md:text-6xl text-center">Empowering Tomorrow&apos;s Leaders</h1>
+                <p className="text-lg md:text-2xl text-center">Discover boundless opportunities for growth and
+                    exploration at Santa
+                    Rosa Science and Technology High School.
+                    Our institution in Santa Rosa, Laguna, is dedicated to fostering innovation and nurturing young
+                    minds to become the trailblazers of the future in science and technology. Join us in shaping a world
+                    where possibilities are limitless.</p>
+            </div>
+        </div>
     );
 }
