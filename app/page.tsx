0193c3897d489fe28ab27b95887c594637508ff6
Kian Gabriel Arambulo <76200573+bg117@users.noreply.github.com>
2024-08-07 21:28:29 +0800

    make header container sized

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,19 @@ export default function Home() {
         <div className="flex flex-col min-h-screen">
             <Header/>
             <div
-                className="flex flex-col flex-1 gap-4 bg-gradient-to-tr from-brand-blue-darker to-brand-red-darker justify-center items-center text-white p-10">
-                <Logo height={128} width={128}/>
-                <h1 className="text-4xl md:text-6xl text-center">Empowering Tomorrow&apos;s Leaders</h1>
-                <p className="text-lg md:text-2xl text-center">Discover boundless opportunities for growth and
-                    exploration at Santa
-                    Rosa Science and Technology High School.
-                    Our institution in Santa Rosa, Laguna, is dedicated to fostering innovation and nurturing young
-                    minds to become the trailblazers of the future in science and technology. Join us in shaping a world
-                    where possibilities are limitless.</p>
+                className="flex-1 flex bg-gradient-to-tr from-brand-blue-darker to-brand-red-darker">
+                <div className="flex-1 flex flex-col gap-4 justify-center items-center text-white p-10 container">
+                    <Logo height={128} width={128}/>
+                    <h1 className="text-4xl md:text-6xl text-center mt-6">Empowering Tomorrow&apos;s Leaders</h1>
+                    <p className="text-lg md:text-2xl text-center mb-6">Discover boundless opportunities for growth and
+                        exploration at Santa
+                        Rosa Science and Technology High School.
+                        Our institution in Santa Rosa, Laguna, is dedicated to fostering innovation and nurturing young
+                        minds to become the trailblazers of the future in science and technology. Join us in shaping a
+                        world
+                        where possibilities are limitless.</p>
+                    <hr className="border-t border-white w-full lg:w-1/2"/>
+                </div>
             </div>
         </div>
     );
--- components/Header.tsx
+++ components/Header.tsx
@@ -15,9 +15,11 @@ const rightLinks = [
 
 export function Header() {
     return (
-        <header className="flex py-4 w-full shrink-0 items-center px-6 border-b bg-brand-blue-default">
-            <MainNav leftLinks={leftLinks} rightLinks={rightLinks}/>
-            <MobileNav leftLinks={leftLinks} rightLinks={rightLinks}/>
+        <header className="flex w-full shrink-0 items-center px-6 bg-brand-blue-default">
+            <div className="py-4 px-6 items-center container flex">
+                <MainNav leftLinks={leftLinks} rightLinks={rightLinks}/>
+                <MobileNav leftLinks={leftLinks} rightLinks={rightLinks}/>
+            </div>
         </header>
     );
 }
