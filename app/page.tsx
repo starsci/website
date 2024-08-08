75a4e65ebf9ba5bf00ee75781c7f86a5a12000c9
Kian Gabriel Arambulo <76200573+bg117@users.noreply.github.com>
2024-08-08 20:26:47 +0800

    add contact page

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import {Card, CardContent} from "@/components/ui/card";
 import {UrlObject} from "node:url";
 import {Footer} from "@/components/Footer";
+import {Page} from "@/components/Page";
 
 type HeroButtonProps = {
     children: React.ReactNode;
@@ -42,46 +43,42 @@ function HeroButton({children, href}: HeroButtonProps) {
 
 export default function Home() {
     return (
-        <>
-            <div className="flex flex-col min-h-screen">
-                <Header/>
-                <main
-                    className="flex-grow flex-shrink-0 flex bg-gradient-to-tr from-brand-blue-darker to-brand-red-darker">
-                    <div className="flex-1 flex flex-col gap-4 justify-center items-center text-white p-10 container">
-                        <Logo height={128} width={128}/>
-                        <h1 className="text-4xl md:text-6xl text-center mt-6">Empowering Tomorrow&apos;s Leaders</h1>
-                        <p className="text-lg md:text-2xl text-center mb-6">Discover boundless opportunities for growth
-                            and
-                            exploration at Santa
-                            Rosa Science and Technology High School.
-                            Our institution in Santa Rosa, Laguna, is dedicated to fostering innovation and nurturing
-                            young
-                            minds to become the trailblazers of the future in science and technology. Join us in shaping
-                            a
-                            world
-                            where possibilities are limitless.</p>
-                        <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-                            <Button size="lg"
-                                    className="w-full text-white transition-colors bg-brand-blue-default hover:bg-brand-blue-darker">
-                                <Link href="/services" className="flex items-center">
-                                    <FontAwesomeIcon icon={faScrewdriverWrench} className="mr-3 h-6 w-6"/>
-                                    Explore Our Services
-                                </Link>
-                            </Button>
-                            <HeroButton href="/contact">
-                                <FontAwesomeIcon icon={faHandshake} className="mr-3 h-6 w-6"/>
-                                Get in Touch
-                            </HeroButton>
-                            <HeroButton href="/about">
-                                <FontAwesomeIcon icon={faClockRotateLeft} className="mr-3 h-6 w-6"/>
-                                About Us
-                            </HeroButton>
-                        </div>
+        <Page>
+            <main
+                className="flex-grow flex-shrink-0 flex bg-gradient-to-tr from-brand-blue-darker to-brand-red-darker">
+                <div className="flex-1 flex flex-col gap-4 justify-center items-center text-white p-10 container">
+                    <Logo height={128} width={128}/>
+                    <h1 className="text-4xl md:text-6xl text-center mt-6">Empowering Tomorrow&apos;s Leaders</h1>
+                    <p className="text-lg md:text-2xl text-center mb-6">Discover boundless opportunities for growth
+                        and
+                        exploration at Santa
+                        Rosa Science and Technology High School.
+                        Our institution in Santa Rosa, Laguna, is dedicated to fostering innovation and nurturing
+                        young
+                        minds to become the trailblazers of the future in science and technology. Join us in shaping
+                        a
+                        world
+                        where possibilities are limitless.</p>
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <Button size="lg"
+                                className="w-full text-white transition-colors bg-brand-blue-default hover:bg-brand-blue-darker">
+                            <Link href="/services" className="flex items-center">
+                                <FontAwesomeIcon icon={faScrewdriverWrench} className="mr-3 h-6 w-6"/>
+                                Explore Our Services
+                            </Link>
+                        </Button>
+                        <HeroButton href="/contact">
+                            <FontAwesomeIcon icon={faHandshake} className="mr-3 h-6 w-6"/>
+                            Get in Touch
+                        </HeroButton>
+                        <HeroButton href="/about">
+                            <FontAwesomeIcon icon={faClockRotateLeft} className="mr-3 h-6 w-6"/>
+                            About Us
+                        </HeroButton>
                     </div>
-                </main>
-            </div>
-            <section
-                className="bg-neutral-800">
+                </div>
+            </main>
+            <section>
                 <div className="px-8 py-14 w-full text-white flex flex-col gap-8">
                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 container">
                         <div className="flex flex-col gap-4">
@@ -99,7 +96,6 @@ export default function Home() {
                     </div>
                 </div>
             </section>
-            <Footer/>
-        </>
+        </Page>
     );
 }
