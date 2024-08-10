5f5c3be62b12101c946e88def951c7afc3a62d66
Kian Gabriel Arambulo <76200573+bg117@users.noreply.github.com>
2024-08-10 15:45:53 +0800

    create news, services, and clubs skeleton files

---
+++ app/news/page.tsx
@@ -0,0 +1,10 @@
+import {Page} from "@/components/Page";
+
+export default function News() {
+    return (
+        <Page>
+            <>
+            </>
+        </Page>
+    )
+}
---
+++ app/services/page.tsx
@@ -0,0 +1,10 @@
+import {Page} from "@/components/Page";
+
+export default function Services() {
+    return (
+        <Page>
+            <>
+            </>
+        </Page>
+    )
+}
