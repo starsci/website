5a5ff4f3361fba964503d2bd226c6cccaaed5869
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-15 18:42:05 +0800

    remove cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import {setupDevPlatform} from '@cloudflare/next-on-pages/next-dev'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // configure picsum.photos as a domain
@@ -13,10 +11,6 @@ const nextConfig = {
       }
     ]
   }
-}
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform()
 }
 
 export default nextConfig
