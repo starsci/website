2bbcafd8ddf7420cd129f39065efcc9574b959a9
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-15 16:45:27 +0800

    use pnpm and add cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // configure picsum.photos as a domain
-    images: {
-        domains: ['picsum.photos'],
-    },
+  // configure picsum.photos as a domain
+  images: {
+    domains: ["picsum.photos"],
+  },
 };
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
