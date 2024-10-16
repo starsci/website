c2c2ae5baa066cbd69efab07868c6090a2927631
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-16 19:04:03 +0800

    implement club directory with fetching

---
+++ app/clubs/actions.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import {createClient} from '@/utils/supabase/server'
+import {v2} from 'cloudinary'
+
+export type Club = {
+  id: number
+  name: string
+}
+
+export async function getClubs() {
+  const supabase = createClient()
+
+  // fetch clubs from clubs table
+  const {error, data} = await supabase.from('clubs').select('*').order('name')
+
+  // throw error if there is any
+  if (error) {
+    throw error
+  }
+
+  return data as Club[]
+}
+
+export async function getClubLogoUrl(
+  clubId: number,
+  width = 100,
+  height = 100
+) {
+  // retrieve cloudinary/clubs/club.id from the club id
+  return v2.url(`clubs/${clubId}`, {
+    width,
+    height,
+    format: 'webp',
+    gravity: 'face',
+    crop: 'fill'
+  })
+}
--- app/clubs/page.tsx
+++ app/clubs/page.tsx
@@ -1,3 +1,48 @@
-export default function ClubDirectory() {
-  return <main></main>
+import Link from 'next/link'
+import {Card, CardContent, CardHeader} from '../ui/card'
+import {getClubLogoUrl, getClubs} from './actions'
+import Image from 'next/image'
+
+export default async function ClubDirectory() {
+  const clubs = await getClubs()
+
+  return (
+    <main>
+      <h1 className="text-4xl font-bold text-center mb-6">
+        Clubs and Organizations
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {clubs.map(async club => {
+          const url = await getClubLogoUrl(club.id)
+
+          return (
+            <div
+              className="relative transition-transform hover:scale-105"
+              key={club.id}>
+              <Card className="shadow-md flex flex-col items-center justify-center">
+                <CardHeader>
+                  <Image
+                    src={url}
+                    alt={`${club.name} logo`}
+                    width={100}
+                    height={100}
+                  />
+                </CardHeader>
+                <CardContent>
+                  <h2 className="text-xl font-semibold text-center">
+                    {club.name}
+                  </h2>
+                </CardContent>
+              </Card>
+              <Link
+                href={`/clubs/${club.id}`}
+                prefetch={false}
+                className="absolute inset-0 z-10"
+              />
+            </div>
+          )
+        })}
+      </div>
+    </main>
+  )
 }
