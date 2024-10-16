6cc38d2d3dace6e01c0e525eb22e3417c1d3b215
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-16 16:11:37 +0800

    set text color to default

--- app/section.tsx
+++ app/section.tsx
@@ -1,6 +1,6 @@
 export function Section({children}: {children: React.ReactNode}) {
   return (
-    <section className="px-4 lg:px-8 py-8 lg:py-14 w-full text-white flex flex-col gap-8">
+    <section className="px-4 lg:px-8 py-8 lg:py-14 w-full flex flex-col gap-8">
       {children}
     </section>
   )
