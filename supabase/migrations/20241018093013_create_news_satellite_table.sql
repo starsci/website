b143c80e093685b798c2a8baca3b0d58cb5695e9
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 17:31:37 +0800

    create newspaper tables migration

---
+++ supabase/migrations/20241018093013_create_news_satellite_table.sql
@@ -0,0 +1,9 @@
+create table news_satellite
+(
+    id          bigint primary key generated always as identity,
+    title       text                                   not null,
+    authors     text[] not null,
+    createdAt   timestamp with time zone default now() not null,
+    body        text                                   not null,
+    thumbnailId text                                   not null
+);
