65f8252cdb165ed34f21f76009935da00c524100
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 14:25:53 +0800

    format migration create_clubs

--- supabase/migrations/20241018060515_create_clubs_table.sql
+++ supabase/migrations/20241018060515_create_clubs_table.sql
@@ -1,6 +1,7 @@
-create table clubs (
-  id bigint primary key generated always as identity,
-  name text not null,
-  description text not null,
-  logoId text not null
+create table clubs
+(
+    id          bigint primary key generated always as identity,
+    name        text not null,
+    description text not null,
+    logoId      text not null
 );
