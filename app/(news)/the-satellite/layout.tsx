ccc2e91de4ac16fd9ce9d1ecbda768f661cc079b
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 21:53:35 +0800

    add The Satellite placeholder layout

--- app/(news)/the-satellite/layout.tsx
+++ app/(news)/the-satellite/layout.tsx
@@ -1,11 +1,24 @@
 import {Header} from '@/components/header'
 import {Footer} from '@/components/footer'
 
+const leftLinks = [
+  {name: 'News', href: '#'},
+  {name: 'Sports', href: '#'},
+  {name: 'Editorial', href: '#'}
+]
+
+const rightLinks = [{name: 'Back to Home', href: '/'}]
+
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      <div> Hello</div>
+      <Header
+        basePath="/the satellite"
+        leftLinks={leftLinks}
+        rightLinks={rightLinks}
+        publicId="pararayos-logo_mrdspq"
+        logoAlt="The Satellite"
+      />
       <div className="flex-grow container p-6">{children}</div>
       <Footer />
     </div>
