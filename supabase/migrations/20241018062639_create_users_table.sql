50aa2a545895724705547c6d04c01e2f0e0eeab1
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 20:25:19 +0800

    remove unnecessary tables

--- supabase/migrations/20241018062639_create_users_table.sql
+++ supabase/migrations/20241018062639_create_users_table.sql
@@ -2,7 +2,9 @@
 -- roles (multiple, FK to roles table), and club memberships (multiple, FK to clubs table)
 create table users
 (
-    id        bigint primary key generated always as identity,
+    id         bigint primary key generated always as identity,
     first_name text not null,
-    last_name  text not null
+    last_name  text not null,
+    auth_id    uuid not null,
+    foreign key (auth_id) references auth.users (id)
 );
--- supabase/migrations/20241018092507_alter_users_connect_to_auth_record.sql
+++
@@ -1,5 +0,0 @@
--- alter users table to create a foreign key from connect to auth.users.id
-alter table users
-    add column auth_id uuid not null,
-    add constraint connect_fk
-        foreign key (auth_id) references auth.users (id);
