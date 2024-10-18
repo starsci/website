22669a5abc80ef2f91a8c56c26f593614810469b
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 21:31:18 +0800

    add default left links to header

--- components/header.tsx
+++ components/header.tsx
@@ -1,19 +1,25 @@
 import {MobileNav} from './mobile-nav.tsx'
 import {MainNav} from './main-nav.tsx'
 
-const leftLinks = [
+const defaultLeftLinks = [
   {name: 'Home', href: '/'},
   {name: 'News', href: '/news'},
   {name: 'Club Directory', href: '/clubs'}
 ]
 
-const rightLinks = [
+const defaultRightLinks = [
   {name: 'Services', href: '/services'},
   {name: 'About', href: '/about'},
   {name: 'Contact', href: '/contact'}
 ]
 
-export function Header() {
+export function Header({
+  leftLinks = defaultLeftLinks,
+  rightLinks = defaultRightLinks
+}: {
+  leftLinks?: {name: string; href: string}[]
+  rightLinks?: {name: string; href: string}[]
+}) {
   return (
     <header className="flex w-full shrink-0 items-center bg-brand-blue-default">
       <div className="py-4 px-6 items-center container flex">
