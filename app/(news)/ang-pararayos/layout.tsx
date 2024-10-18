89e0c82a4673e4f8b74a4407448797c3a2679f29
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 21:42:00 +0800

    add Pararayos header links

--- app/(news)/ang-pararayos/layout.tsx
+++ app/(news)/ang-pararayos/layout.tsx
@@ -1,10 +1,23 @@
 import {Header} from '@/components/header'
 import {Footer} from '@/components/footer'
 
+const leftLinks = [{name: 'Home', href: '/'}]
+
+const rightLinks = [
+  {name: 'Lathalain', href: '#'},
+  {name: 'Isports', href: '#'},
+  {name: 'Editoryal', href: '#'}
+]
+
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header publicId="pararayos-logo_mrdspq" logoAlt="Ang Pararayos" />
+      <Header
+        leftLinks={leftLinks}
+        rightLinks={rightLinks}
+        publicId="pararayos-logo_mrdspq"
+        logoAlt="Ang Pararayos"
+      />
       <div className="flex-grow container p-6">{children}</div>
       <Footer />
     </div>
