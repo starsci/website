793d56df0e6b9548b44318e6c6ddb1a10061740f
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 21:36:42 +0800

    replace Pararayos layout header logo

--- app/(news)/ang-pararayos/layout.tsx
+++ app/(news)/ang-pararayos/layout.tsx
@@ -4,8 +4,7 @@ import {Footer} from '@/components/footer'
 export default function Layout({children}: {children: React.ReactNode}) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-      <div> Hello</div>
+      <Header publicId="pararayos-logo_mrdspq" logoAlt="Ang Pararayos" />
       <div className="flex-grow container p-6">{children}</div>
       <Footer />
     </div>
