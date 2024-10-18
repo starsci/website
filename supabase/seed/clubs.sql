6750101ff74e93b4f7d6cf3cb77f122f2c36d48a
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 17:59:17 +0800

    seed clubs

---
+++ supabase/seed/clubs.sql
@@ -0,0 +1,18 @@
+-- seed 5 clubs with logo_id: srsths-logo_p3p9be
+
+insert into clubs (name, description, logo_id)
+values ('Supreme Student Government',
+        'The Supreme Student Government is the highest governing body of the student body.',
+        'srsths-logo_p3p9be'),
+       ('Science Club',
+        'The Science Club is a club for students who are interested in science.',
+        'srsths-logo_p3p9be'),
+       ('Math Club',
+        'The Math Club is a club for students who are interested in math.',
+        'srsths-logo_p3p9be'),
+       ('English Club',
+        'The English Club is a club for students who are interested in English.',
+        'srsths-logo_p3p9be'),
+       ('Filipino Club',
+        'The Filipino Club is a club for students who are interested in Filipino.',
+        'srsths-logo_p3p9be');
