d78bc1187ce5f91e6c16ab98fc0c1be674e28422
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-18 17:49:53 +0800

    remove default from users.auth_id

--- supabase/migrations/20241018092507_alter_users_connect_to_auth_record.sql
+++ supabase/migrations/20241018092507_alter_users_connect_to_auth_record.sql
@@ -1,5 +1,5 @@
 -- alter users table to create a foreign key from connect to auth.users.id
 alter table users
-    add column auth_id uuid not null default gen_random_uuid(),
+    add column auth_id uuid not null,
     add constraint connect_fk
         foreign key (auth_id) references auth.users (id);
