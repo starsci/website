0c2070c2788556c4bce8cbc134a44858feafb487
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-23 10:32:58 +0800

    move data fetching to new file

--- app/(app)/(front)/announcements-section.tsx
+++ app/(app)/(front)/announcements-section.tsx
@@ -1,60 +1,6 @@
-import {NewsSection} from '../../../components/news-section'
+import {NewsSection} from '@/components/news-section'
 import {Section} from '@/components/section'
-
-import {getPayloadHMR} from '@payloadcms/next/utilities'
-import config from '@payload-config'
-
-const payload = await getPayloadHMR({config})
-
-async function getAnnouncements() {
-  'use server'
-
-  // fetch clubs from clubs table
-  const data = await payload.find({
-    collection: 'school-announcements',
-    pagination: true,
-    limit: 5,
-    sort: '-published_at'
-  })
-
-  console.log(data)
-
-  return data
-}
-
-async function getSatellite() {
-  'use server'
-
-  // fetch clubs from clubs table
-  const data = await payload.find({
-    collection: 'the-satellite-news',
-    pagination: true,
-    limit: 5,
-    sort: '-published_at'
-  })
-
-  console.log(data)
-
-  return data
-}
-
-
-async function getPararayos() {
-  'use server'
-
-  // fetch clubs from clubs table
-  const data = await payload.find({
-    collection: 'ang-pararayos-news',
-    pagination: true,
-    limit: 5,
-    sort: '-published_at'
-  })
-
-  console.log(data)
-
-  return data
-}
-
+import {getAnnouncements, getPararayos, getSatellite} from './actions'
 
 export async function AnnouncementsSection() {
   const announcements = await getAnnouncements()
