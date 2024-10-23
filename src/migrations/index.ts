c9e5af59c2d05a89940b05281dbf278b1b49c70b
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-23 09:25:29 +0800

    create migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20241023_012507 from './20241023_012507';
+
+export const migrations = [
+  {
+    up: migration_20241023_012507.up,
+    down: migration_20241023_012507.down,
+    name: '20241023_012507'
+  },
+];
