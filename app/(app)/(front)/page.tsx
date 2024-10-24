0cb654a681f9d311dbf0a89e600152fbb7937146
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-24 20:29:32 +0800

    use custom HR component

--- app/(app)/(front)/page.tsx
+++ app/(app)/(front)/page.tsx
@@ -1,13 +1,14 @@
 import {Hero} from '@/components/hero-sections/hero'
 import {Services} from '@/components/hero-sections/services'
 import {Announcements} from '@/components/hero-sections/announcements'
+import {HR} from '@/components/hr'
 
 export default async function Home() {
   return (
     <div>
       <Hero />
       <Services />
-      <hr className="border-t border-gray-300" />
+      <HR />
       <Announcements />
     </div>
   )
