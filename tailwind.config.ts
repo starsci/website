cee413f22f5bacea2244e7e7c0571a1bbd797714
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-24 21:59:20 +0800

    display announcement slug

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -96,7 +96,7 @@ const config = {
       }
     }
   },
-  plugins: [require('tailwindcss-animate')]
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')]
 } satisfies Config
 
 export default config
