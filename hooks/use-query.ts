03a168ac5d063bc299712a18ad6c27c7f1c3379c
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-29 23:46:08 +0800

    feat: use react-query instead of swr in use-query

--- hooks/use-query.ts
+++ hooks/use-query.ts
@@ -1,6 +1,6 @@
-import useSWR from 'swr'
 import {CollectionSlug, PaginatedDocs, DataFromCollectionSlug} from 'payload'
 import {queryCollection} from './server/payload-query'
+import {useQuery as useReactQuery} from '@tanstack/react-query'
 
 // basically does a payload query but using useSWR and automatically casts
 // the return type to the collection's type
@@ -8,7 +8,8 @@ export function useQuery<TSlug extends CollectionSlug>(
   collection: TSlug,
   options: Omit<Parameters<typeof queryCollection>[1], 'collection'>
 ) {
-  return useSWR<PaginatedDocs<DataFromCollectionSlug<TSlug>>>(collection, () =>
-    queryCollection(collection, options)
-  )
+  return useReactQuery<PaginatedDocs<DataFromCollectionSlug<TSlug>>>({
+    queryKey: [collection, options],
+    queryFn: () => queryCollection(collection, options)
+  })
 }
