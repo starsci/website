f2dc8aa7f3804364c3513500ada679cf2f3bbc43
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-10-29 23:52:46 +0800

    refactor: update useQuery function to accept options object

--- hooks/use-query.ts
+++ hooks/use-query.ts
@@ -1,15 +1,18 @@
 import {CollectionSlug, PaginatedDocs, DataFromCollectionSlug} from 'payload'
-import {queryCollection} from './server/payload-query'
+import {Options, queryCollection} from './server/payload-query'
 import {useQuery as useReactQuery} from '@tanstack/react-query'
 
 // basically does a payload query but using useSWR and automatically casts
 // the return type to the collection's type
 export function useQuery<TSlug extends CollectionSlug>(
-  collection: TSlug,
-  options: Omit<Parameters<typeof queryCollection>[1], 'collection'>
+  options:
+    | {
+        collection: TSlug
+      }
+    | Options<TSlug>
 ) {
   return useReactQuery<PaginatedDocs<DataFromCollectionSlug<TSlug>>>({
-    queryKey: [collection, options],
-    queryFn: () => queryCollection(collection, options)
+    queryKey: [options.collection, options],
+    queryFn: () => queryCollection(options)
   })
 }
