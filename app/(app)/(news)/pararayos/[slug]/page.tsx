415886c151a351bb0345bb85b41ac8b6de0c8c28
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-12-25 22:02:20 +0800

    feat: create slug pages for Pararayos and The Satellite

---
+++ app/(app)/(news)/pararayos/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import { Aside } from "@/components/aside"
+import { Content } from "@/components/content"
+
+type Params = { params: Promise<{ slug: string }> }
+
+export default async function Announcement({ params }: Params) {
+  const { slug } = await params
+  return (
+    <div className="flex flex-row lg:flex-row-reverse space-x-8 lg:space-x-reverse lg:justify-end">
+      <div className="md:w-2/3 lg:w-1/2 w-full">
+        <Content slug={slug} collection="ang-pararayos-news" />
+      </div>
+      <div className="md:w-1/3 lg:w-1/4 md:block hidden">
+        <Aside slug={slug} collection="ang-pararayos-news" caption="News" />
+      </div>
+    </div>
+  )
+}
+
+export async function generateMetadata({ params }: Params) {
+  const { slug } = await params
+  const data = await fetch(`${process.env.API_BASE || ''}/api/ang-pararayos-news/${slug}`)
+  const { title, bodyHTML } = await data.json()
+  const description = bodyHTML.replace(/<\s*br[^>]?>/gm, '\n')
+    .replace(/(<([^>]+)>)/gm, "").slice(0, 160)
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'article',
+      url: `${process.env.API_BASE || ''}/announcements/${slug}`,
+      site_name: 'Santa Rosa Science and Technology High School',
+    }
+  }
+}
