c8b34c2c1047b9817ff895b272597fbd289f4ce2
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-12-25 13:30:31 +0800

    feat: add front page announcement

--- app/(app)/(front)/page.tsx
+++ app/(app)/(front)/page.tsx
@@ -1,11 +1,13 @@
-import {Hero} from '@/components/hero-sections/Hero'
-import {CampusNews} from '@/components/hero-sections/CampusNews'
-import {HR} from '@/components/HR'
+import { Hero } from '@/components/hero-sections/Hero'
+import { CampusNews } from '@/components/hero-sections/CampusNews'
+import { HR } from '@/components/HR'
+import { Services } from '@/components/hero-sections/Services'
 
 export default async function Home() {
   return (
     <div>
       <Hero />
+      <Services />
       <HR />
       <CampusNews />
     </div>
--- components/hero-sections/Services.tsx
+++ components/hero-sections/Services.tsx
@@ -1,29 +1,65 @@
-import {ServiceCard} from '@/components/ServiceCard'
-import {HeroSection} from '@/components/HeroSection'
-import {FilePen, GraduationCap, HeartHandshake} from 'lucide-react'
+'use client'
+
+import { useQuery } from '@/hooks/use-query'
+import { Media } from '@/payload-types'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export function Services() {
+  const { data, isLoading, error } = useQuery({
+    collection: 'school-announcements',
+    where: {
+      frontPage: {
+        equals: true
+      }
+    }
+  })
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+
+  if (error) {
+    return <p>Failed to load front page announcement: {error.message}</p>
+  }
+
+  const [frontPage] = data?.docs || []
+
+  if (!frontPage) {
+    return <></>
+  }
+
   return (
-    <HeroSection>
-      <h2 className="text-2xl text-center font-semibold">School Services</h2>
-      <p className="text-lg text-center lg:w-1/2 md:mx-auto">
-        Discover the wide range of services we offer to support our
-        students&apos; academic and personal growth.
-      </p>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-8 mt-4 place-items-center">
-        <ServiceCard icon={<FilePen />} title="Registrar Services">
-          Learn more about our registrar services, including enrollment,
-          records, and more.
-        </ServiceCard>
-        <ServiceCard icon={<HeartHandshake />} title="Counseling Services">
-          Explore our counseling services, including guidance, mental health,
-          and more.
-        </ServiceCard>
-        <ServiceCard icon={<GraduationCap />} title="Academic Services">
-          Discover our academic services, including tutoring, scholarships, and
-          more.
-        </ServiceCard>
+    <div className="flex flex-col sm:flex-row gap-4 lg:px-16 py-8">
+      {frontPage.thumbnail && (
+        <Image
+          src={(frontPage.thumbnail as Media).cdn_url!}
+          alt={`${frontPage.title}`}
+          width={0}
+          height={0}
+          className="sm:w-1/3 w-full object-contain mb-auto"
+          sizes="100vw"
+        />
+      )}
+      <div className="flex flex-col">
+        <h3 className="text-lg leading-6">
+          <Link className="hover:underline" href={`/announcements/${frontPage.id}`}>
+            {frontPage.title}
+          </Link>
+        </h3>
+        {/* <small className="text-sm font-semibold text-neutral-400">
+                {
+                  // join the array of authors with a comma and a space
+                  frontPage.authors.join(', ')
+                }
+              </small> */}
+        <small className="text-sm text-neutral-400">
+          {new Date(frontPage.createdAt).toLocaleString()}
+        </small>
+        <p
+          className="text-sm line-clamp-4 md:line-clamp-[8] prose-sm"
+          dangerouslySetInnerHTML={{ __html: frontPage.bodyHTML! }}></p>
       </div>
-    </HeroSection>
+    </div>
   )
 }
