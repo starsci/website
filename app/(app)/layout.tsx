1b53805a8fee7e5a9872c682b02f963fc6c522e0
Kian Gabriel Arambulo <kiangabrielarambulo@gmail.com>
2024-12-26 15:22:54 +0800

    feat: add image to OpenGraph metadata

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -4,12 +4,33 @@ import { Toaster } from '@/components/ui/toaster'
 import { ReactQueryClientProvider } from '@/components/QueryClientProvider'
 
 import './globals.css'
+import { v2 } from 'cloudinary'
 
 const nextFont = PT_Sans({ weight: ['400', '700'], subsets: ['latin'] })
+const title = 'Santa Rosa Science and Technology High School'
+const description = 'Public science and technology high school in Santa Rosa, Laguna'
 
 export const metadata: Metadata = {
-  title: 'Santa Rosa Science and Technology High School',
-  description: 'Public science and technology high school in Santa Rosa, Laguna'
+  title,
+  description,
+  openGraph: {
+    title,
+    description,
+    type: 'website',
+    url: process.env.API_BASE || '',
+    siteName: 'Santa Rosa Science and Technology High School',
+    images: [
+      {
+        url: v2.url(process.env.NEXT_PUBLIC_HERO_PUBLIC_ID || '', {
+          width: 1200,
+          height: 630,
+          crop: 'fill'
+        }),
+        width: 1200,
+        height: 630,
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
--- lib/generateMetadata.ts
+++ lib/generateMetadata.ts
@@ -1,18 +1,24 @@
+import {ResolvingMetadata} from 'next'
+
 export function metadataFunction(
   collection: string,
   path: string,
   slugName: string = 'slug'
 ) {
-  return async function ({params}: {params: Promise<{[key: string]: string}>}) {
+  return async function (
+    {params}: {params: Promise<{[key: string]: string}>},
+    parent: ResolvingMetadata
+  ) {
     const slug = (await params)[slugName]
     const data = await fetch(
       `${process.env.API_BASE}/api/${collection}/${slug}`
     )
-    const {title, bodyHTML} = await data.json()
+    const {title, bodyHTML, thumbnail} = await data.json()
     const description = bodyHTML
       .replace(/<\s*br[^>]?>/gm, '\n')
       .replace(/(<([^>]+)>)/gm, '')
       .slice(0, 160)
+    const thumbnailUrl = thumbnail?.cdn_url
 
     return {
       title,
@@ -22,7 +28,8 @@ export function metadataFunction(
         description,
         type: 'article',
         url: `${process.env.API_BASE}/${path}/${slug}`,
-        siteName: 'Santa Rosa Science and Technology High School'
+        siteName: 'Santa Rosa Science and Technology High School',
+        images: [thumbnailUrl, ...((await parent).openGraph?.images || [])]
       }
     }
   }
